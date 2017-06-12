c42d77bde9821c8dbe2d3c82111023a1a4b4fd3b
Zhou Peng <zhoupeng@tospur.com>
2017-06-12 16:55:43 +0800

    add namespace=ambari

--- config-ssh-k8s.sh
+++ config-ssh-k8s.sh
@@ -16,16 +16,20 @@ run_command() {
   fi
 }
 
+_kubectl(){
+    kubectl --namespace=ambari $@
+}
+
 _get_amb_server_name(){
-    kubectl get pod --namespace=ambari -o wide | grep ambari-server | awk '{print $1}'
+    _kubectl get pod -o wide | grep ambari-server | awk '{print $1}'
 }
 
 _get_amb_agents_name(){
-    kubectl get pod --namespace=ambari -o wide | grep amb-agent | awk '{print $1}'
+    _kubectl get pod -o wide | grep amb-agent | awk '{print $1}'
 }
 
 _get_amb_agents_ip(){
-    kubectl get pod --namespace=ambari -o wide | grep amb-agent | awk '{print $6}'
+    _kubectl get pod -o wide | grep amb-agent | awk '{print $6}'
 }
 
 _amb_server_ssh_keygen(){
@@ -35,13 +39,13 @@ _amb_server_ssh_keygen(){
 _amb_copy_ssh_to_agent(){
     local host_name=${1:?"Usage: _amb_copy_ssh_to_agent <host_name> <server-name> "}
     local ambari_server_name=${2:?"Usage: _amb_copy_ssh_to_agent <host_name> <server-name>"}
-    run_command kubectl exec $ambari_server_name  sh -c "ssh-keyscan $host_name >> ~/.ssh/known_hosts"
-    run_command kubectl exec $ambari_server_name  sh -c "sshpass -p Zasd_1234 ssh-copy-id root@${host_name}"
+    run_command _kubectl exec $ambari_server_name  sh -c "ssh-keyscan $host_name >> ~/.ssh/known_hosts"
+    run_command _kubectl exec $ambari_server_name  sh -c "sshpass -p Zasd_1234 ssh-copy-id root@${host_name}"
 }
 
 config_master(){
     local ambari_server_name=$(_get_amb_server_name)
-    run_command kubectl exec $ambari_server_name  sh -c "echo -e  'y\n'|ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa"
+    run_command _kubectl exec $ambari_server_name  sh -c "echo -e  'y\n'|ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa"
 
     for i in $(_get_amb_agents_ip); do
         run_command _amb_copy_ssh_to_agent $i $ambari_server_name
@@ -58,12 +62,12 @@ config_agents(){
 _amb_start_agent_service() {
   local agent_name=${1:?"Usage: _amb_start_agent_service <agent_name>"}
   # set password to agent, for server ssh
-  run_command kubectl exec $agent_name sh -c " echo Zasd_1234 | passwd root --stdin "
-  run_command kubectl exec $agent_name sh -c " systemctl restart ntpd "
+  run_command _kubectl exec $agent_name sh -c " echo Zasd_1234 | passwd root --stdin "
+  run_command _kubectl exec $agent_name sh -c " systemctl restart ntpd "
 }
 
 amb_tool_get_server_sshkey() {
-  kubectl exec $(_get_amb_server_name)  sh -c "cat ~/.ssh/id_rsa"
+  _kubectl exec $(_get_amb_server_name)  sh -c "cat ~/.ssh/id_rsa"
 }
 
 amb_tool_get_agent_host_list() {
