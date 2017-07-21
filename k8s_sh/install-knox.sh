6ef068996d15e42241076991394fa239c55b8582
Zhou Peng <zhoupeng@tospur.com>
2017-07-21 11:16:53 +0800

    knox config

--- k8s_sh/install-knox.sh
+++ k8s_sh/install-knox.sh
@@ -37,3 +37,41 @@ vi /usr/hdp/current/ranger-admin/db/mysql/create_dbversion_catalog.sql
 (updated_at      timestamp not null default current_timestamp,)
 ]
 
+
+[Install Ranger KMS]
+1.Mysql db host: amb-mysql
+
+2.Add values for the following properties in the "Custom kms-site" section
+
+hadoop.kms.proxyuser.hive.users
+hadoop.kms.proxyuser.oozie.users
+hadoop.kms.proxyuser.HTTP.users
+hadoop.kms.proxyuser.ambari.users
+hadoop.kms.proxyuser.yarn.users
+hadoop.kms.proxyuser.hive.hosts
+hadoop.kms.proxyuser.oozie.hosts
+hadoop.kms.proxyuser.HTTP.hosts
+hadoop.kms.proxyuser.ambari.hosts
+hadoop.kms.proxyuser.yarn.hosts
+
+3.Add the following properties to the Custom KMS-site section of the configuration
+hadoop.kms.proxyuser.keyadmin.groups=*
+hadoop.kms.proxyuser.keyadmin.hosts=*
+hadoop.kms.proxyuser.keyadmin.users=*
+
+
+[Enable HDFS encryption]
+1. At Ranger KMS Host link config file
+sudo ln -s /etc/hadoop/conf/core-site.xml /etc/ranger/kms/conf/
+core-site.xml
+
+2.Configure HDFS to access Ranger KMS.
+Advanced core-site
+    hadoop.security.key.provider.path = kms://http@amb-0.agent.ambari.svc.k8s:9292/kms
+Advanced hdfs-site
+    dfs.encryption.key.provider.uri = kms://http@amb-0.agent.ambari.svc.k8s:9292/kms
+
+3. Under Custom core-site.xml, set the value of the hadoop.proxyuser.kms.groups
+property to * or service user.
+
+4. Restart the Ranger KMS service and the HDFS service.
